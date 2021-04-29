55dbb75040e04466dd93f7ca686df3fb3f65f775
Carlos Martinez <curlosmart@gmail.com>
2021-04-29 10:26:32 -0400

    can enter valid conditional operator

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 document.getElementById('convert-json').addEventListener('click', () => {
     let firstString = document.getElementById('first-string').value.trim();
     let secondString = document.getElementById('second-string').value.trim();
+    let comparisonOperator = document.getElementById('comparison-operator').value;
 
     console.log([firstString]);
     console.log([secondString]);
+    console.log([comparisonOperator]);
 });
 
 
